94292b2359d4062ff40b145048f80b99e132ac18
HsinI Lin <410538242@github.com>
2022-10-19 15:05:48 +0800

    demo

---
+++ js/demo_42.js
@@ -0,0 +1,19 @@
+function showpage(index) {
+
+    const p=document.querySelector('.showclassdemo');
+    console.log('p',p);
+    switch(index){
+        case 1:
+p.innerHTML = `<iframe src="./demo/w02/"width=100%" height=100%" />`
+        break;
+        case 2:
+
+        break;
+        case 3:
+
+        break;
+        case 4:
+
+        break;
+    }
+}
