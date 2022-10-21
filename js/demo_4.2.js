92866fcfe833b7a91ea4f205fcd261583a9dfebb
HsinILin <113498097+410538242@users.noreply.github.com>
2022-10-22 00:18:15 +0800

    Add files via upload

---
+++ js/demo_4.2.js
@@ -0,0 +1,19 @@
+function showpage(index) {
+
+    const p = document.querySelector('.show-classdemo');
+    console.log('p', p);
+    switch(index) {
+        case 1:
+            p.innerHTML = `<iframe src="./demo/w02/w02 index.html" width="100%" height="100%" />`
+        break;
+        case 2:
+            p.innerHTML = `<iframe src="./demo/w03/card_42.html" width="100%" height="100%" />`
+        break;
+        case 3:
+            p.innerHTML = `<iframe src="./demo/w05/blog_42.html" width="100%" height="100%" />`
+        break;
+        case 4:
+            p.innerHTML =`>iframe src="./demo/w06/blog_42.html" width="100%" height="100%"/>`
+        break;
+    }
+}
